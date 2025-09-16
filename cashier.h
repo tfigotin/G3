7a3535b2bea24572c0f2e25fcf41ad4ef8f9d230
LukeTheCodingGenius <lukeleibee@gmail.com>
2025-09-16 15:30:38 -0700

    Added header files

---
+++ cashier.h
@@ -0,0 +1,11 @@
+/****************************************************************
+ * CS1B - G2 : Serendipity Menus (Navigatin & Stubs)            *
+ * Partner A : Tamar Figotin (A00328312) - role: driver         *
+ * Partner B : Luke Leibee   (1248406) - role: function         *
+ * Due Date  : 2025-09-07                                       *
+ * Purpose   : Practice making a multi-file C++ program with    *
+ *					menus, input checks, and a simple cashier slip.  *
+ * Build     : g++ -std=c++20 mainmenu.cpp cashier.cpp          *
+ *					invmenu.cpp reports.cpp -o serendipity &&        *
+ *					./serendipity                                    *
+ ****************************************************************/
