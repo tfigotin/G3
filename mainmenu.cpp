af09746fbb45480ef89417fc7e174ebd770c1efb
LukeTheCodingGenius <lukeleibee@gmail.com>
2025-09-18 15:14:50 -0700

    Create menu code

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -9,3 +9,47 @@
 					invmenu.cpp reports.cpp -o serendipity &&        *
 					./serendipity                                    *
  ****************************************************************/
+
+#include "serendipity.h"
+
+int main()
+{
+	char choice {};
+	string discard;
+
+	do
+	{
+		cout << "\033[H\033[2J" << endl; //Clear the screen
+
+		printMainMenu();
+
+		cin.get(choice);
+
+		switch(choice)
+		{
+			case '1':
+				cashier();
+				break;
+			case '2':
+				invmenu();
+				break;
+			case '3':
+				reports();
+				break;
+			case '4':
+				cout << "You chose option 4" << endl;
+				break;
+			default:
+				cout << "*** ->" << choice << "<- *** is Invalid" << endl;
+				cout << "Please enter a valid choice 1-4" << endl;
+				break;
+			cin.clear();
+
+			cin.ignore();
+			cin.ignore();
+	}while(choice != 4);
+
+
+}
+
+
